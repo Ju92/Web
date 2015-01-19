17a90a25261d64088ecf8c99a069d8f47661b867
Juliette <Juliette@Kira>
2015-01-19 13:58:31 +0100

    Machine à état suite

--- Components/src/MarkingMenuSM.java
+++ Components/src/MarkingMenuSM.java
@@ -3,11 +3,48 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+import fr.lri.swingstates.canvas.CStateMachine;
 
+/*
+    Machine à état contrôlant le comportement du marking menu
+*/
 /**
  *
  * @author Juliette
  */
-public class MarkingMenuSM {
+public class MarkingMenuSM extends CStateMachine {
+   
+    public StripSM(CShape shape, StripView stripView) {
+        super(Desktop.getCanvasInstance());
+        this.leftShape = shape;
+        this.stripView = stripView;
+        points = new ArrayList<>();
+    }
+    /**
+     * START.
+     */
+    public State Start = new State() {
+        // Press of the pen on the left part of the strip
+        Transition penPress = new PressOnShape(BUTTON1, ">> Drawing") {
+            @Override
+            public boolean guard() {
+                return isStylus() && getShape() == leftShape;
+            }
+
+            @Override
+            public void action() {
+                //Initialisation of the drawing
+                lastDrawPoint = getPoint();
+                gesture.addPoint(lastDrawPoint.getX(), lastDrawPoint.getY());
+                polyLine = new CPolyLine(lastDrawPoint);
+                polyLines.add(polyLine);
+                polyLine.setStroke(new BasicStroke(2));
+                polyLine.setOutlinePaint(Color.RED);
+                polyLine.setTransparencyOutline(0.6f);
+                polyLine.setPickable(false);
+                Desktop.getCanvasInstance().addShape(polyLine);
+            }
+        };
+
     
 }
