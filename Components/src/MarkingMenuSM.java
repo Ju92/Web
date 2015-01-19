ac2c520054d7cf2ada80c9e40ec9fe1d4e32714e
Juliette <Juliette@Kira>
2015-01-19 14:06:34 +0100

    Machine à état suite

--- Components/src/MarkingMenuSM.java
+++ Components/src/MarkingMenuSM.java
@@ -3,48 +3,72 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+import fr.lri.swingstates.canvas.CShape;
 import fr.lri.swingstates.canvas.CStateMachine;
+import fr.lri.swingstates.canvas.transitions.PressOnShape;
+import fr.lri.swingstates.sm.State;
+import fr.lri.swingstates.sm.Transition;
 
 /*
-    Machine à état contrôlant le comportement du marking menu
-*/
+ Machine à état contrôlant le comportement du marking menu
+ */
 /**
  *
  * @author Juliette
  */
 public class MarkingMenuSM extends CStateMachine {
-   
-    public StripSM(CShape shape, StripView stripView) {
-        super(Desktop.getCanvasInstance());
-        this.leftShape = shape;
-        this.stripView = stripView;
-        points = new ArrayList<>();
+
+    public MarkingMenuSM(CShape shape) {
+        super();
     }
     /**
-     * START.
+     * INIT.
      */
-    public State Start = new State() {
+    public State Init = new State() {
         // Press of the pen on the left part of the strip
-        Transition penPress = new PressOnShape(BUTTON1, ">> Drawing") {
+        Transition mousePress = new PressOnShape(BUTTON1, ">> Pressing") {
             @Override
             public boolean guard() {
-                return isStylus() && getShape() == leftShape;
+                return false;
             }
 
             @Override
             public void action() {
-                //Initialisation of the drawing
-                lastDrawPoint = getPoint();
-                gesture.addPoint(lastDrawPoint.getX(), lastDrawPoint.getY());
-                polyLine = new CPolyLine(lastDrawPoint);
-                polyLines.add(polyLine);
-                polyLine.setStroke(new BasicStroke(2));
-                polyLine.setOutlinePaint(Color.RED);
-                polyLine.setTransparencyOutline(0.6f);
-                polyLine.setPickable(false);
-                Desktop.getCanvasInstance().addShape(polyLine);
             }
         };
+    };
+    /**
+     * PRESSING.
+     */
+    public State Pressing = new State() {
+        // Press of the pen on the left part of the strip
+        Transition mousePress = new PressOnShape(BUTTON1, ">> Dragging") {
+            @Override
+            public boolean guard() {
+                return false;
+            }
 
-    
+            @Override
+            public void action() {
+            }
+        };
+    };
+    /**
+     * DRAGGING.
+     */
+    public State Dragging = new State() {
+        // Press of the pen on the left part of the strip
+        Transition mousePress = new PressOnShape(BUTTON1, ">> Init") {
+            @Override
+            public boolean guard() {
+                return false;
+            }
+
+            @Override
+            public void action() {
+            }
+        };
+    };
+
 }
